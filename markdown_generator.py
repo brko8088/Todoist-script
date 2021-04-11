45df2ce3a2e792b5d836f0cfc063a9ac4585a45e
BrunoKoppel <bruno.koppel@gmail.com>
2021-04-11 09:20:19 -0400

    refactor: 💡 Clean up

--- markdown_generator.py
+++ markdown_generator.py
@@ -8,9 +8,6 @@ class MarkdownGenerator:
 # Code to build the Markdown generator extension
 # I've left this here for when I decide to consider building it.
 #
-# test_directory = "/Users/brunokoppel/.sj/04 Project (Building the Foundations)/07 Epic (The Spring Semester)/"
-# test_filename = "Sprint_test.md"
-#
 # tree_file = file_manager.Directory(mainApp.user_data.get("main_scrum_directory"))
 # for project_directory in tree_file.sub_directories:
 #     print(project_directory.directory_path + " Subdirectories:")
